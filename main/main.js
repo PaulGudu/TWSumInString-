31ab83ebe361ef33107e8a042a9fd80990742b80
Paul Gudu <3348225661@qq.com>
2017-06-12 19:15:49 +0800

    sum of integers in a string

--- main/main.js
+++ main/main.js
@@ -1,3 +1,10 @@
-module.exports = function main() {
-  // Write your cade here
+module.exports = function main(string) {
+  // Write your code here
+  var array = string.split('');
+  var sum = 0;
+  for (var i = array.length - 1; i >= 0; i--) {
+  	 array[i] = parseInt(string[i]);
+  	 sum += array[i];
+  }
+  return sum; 
 };
